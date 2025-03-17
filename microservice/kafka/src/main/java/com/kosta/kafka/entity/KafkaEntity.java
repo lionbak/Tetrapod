7680a07a173d3592d022dd71d51cf5128faf4400
Jinwoo Bak <jbak7469@gmail.com>
2025-03-17 20:09:03 +0900

    Feat: Kafka Entity 추가

---
+++ microservice/kafka/src/main/java/com/kosta/kafka/entity/KafkaEntity.java
@@ -0,0 +1,15 @@
+package com.kosta.kafka.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaEntity {
+
+    private Long id;
+    private String message;
+
+}
